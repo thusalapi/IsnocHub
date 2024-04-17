11efe7624c94ef46943aba5b28b6bad696367a83
Thusala Piyarisi <49372490+thusalapi@users.noreply.github.com>
2024-04-17 17:58:21 +0530

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+//fun CalCrumCricle(r: Int){
+//    print(2 * (22/7) * r);
+//}
+
+fun main() {
+    var myArry = arrayOf("MAD", "ITP", "PS", "Profs Skills", "ESD", "DSA")
+
+    for (i in myArry){
+        println(i);
+    }
+
+}
